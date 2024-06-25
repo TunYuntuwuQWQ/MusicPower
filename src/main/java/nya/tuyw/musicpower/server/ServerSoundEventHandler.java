09094a661ec34a3ae0a4c515f87097f684972f8b
TunYuntuwuQWQ <tunyuntuwu__0@163.com>
2024-06-25 22:46:57 +0800

    better logic

--- src/main/java/nya/tuyw/musicpower/server/ServerSoundEventHandler.java
+++ src/main/java/nya/tuyw/musicpower/server/ServerSoundEventHandler.java
@@ -8,19 +8,23 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraftforge.registries.ForgeRegistries;
+import nya.tuyw.musicpower.MusicPower;
 import nya.tuyw.musicpower.init.ConfigHandler;
 
 public class ServerSoundEventHandler {
     public static void handleSoundEvent(MinecraftServer server, BlockPos soundPos, ResourceLocation soundType) {
         for (ConfigHandler.EffectConfig effectConfig : ConfigHandler.getEffectConfigs()) {
             if (effectConfig.getSound().equals(soundType.toString())) {
+                MobEffect mobEffect = ForgeRegistries.MOB_EFFECTS.getValue(new ResourceLocation(effectConfig.getBuff()));
+                if (mobEffect == null) {
+                    MusicPower.LOGGER.warn(effectConfig.getBuff()+"-not found. with-"+effectConfig.getSound());
+                    continue;
+                }
+                int buffStrength = effectConfig.getStrength() - 1;
+                int buffDuration = effectConfig.getDuration() * 20;
                 for (ServerPlayer player : server.getPlayerList().getPlayers()) {
                     double distanceSq = player.blockPosition().distSqr(soundPos);
                     if (distanceSq < effectConfig.getDistance() * effectConfig.getDistance()) {
-                        MobEffect mobEffect = ForgeRegistries.MOB_EFFECTS.getValue(new ResourceLocation(effectConfig.getBuff()));
-                        if (mobEffect == null) continue;
-                        int buffStrength = effectConfig.getStrength() - 1;
-                        int buffDuration = effectConfig.getDuration() * 20;
                         if (effectConfig.getStrengthDecaywithDistance() || effectConfig.getDurarionDecaywithDistance()) {
                             double distance = Math.sqrt(distanceSq);
                             double maxDistance = effectConfig.getDistance();
